cd613c612c50f278810d1723c02f889d65e1f1aa
Natanael Pessoa <natanpessoa88@gmail.com>
2020-05-04 15:07:48 -0300

    Projeto criado

---
+++ src/entitys/Principal.java
@@ -0,0 +1,23 @@
+
+package entitys;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        Locale.setDefault(Locale.US);
+        
+        
+        
+        
+        
+        
+    }
+    
+}
