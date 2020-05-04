e33c6601d799b38efff20761a5b8fbdd436d7df2
Natanael Pessoa <natanpessoa88@gmail.com>
2020-05-04 15:35:27 -0300

    Renomeando classes

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,11 +1,11 @@
 
-package entitys;
+package aplication;
 
 import java.util.Locale;
 import java.util.Scanner;
 
 
-public class Principal {
+public class Program {
 
     public static void main(String[] args) {
         
